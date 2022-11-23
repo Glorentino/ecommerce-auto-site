b69191cfd75a517078e3ca5461c6b6d5be8a883f
Chris <Chrisflorentino96@gmail.com>
2022-11-23 13:43:09 -0500

    ChatBot Component

--- store/src/App.js
+++ store/src/App.js
@@ -10,61 +10,9 @@ import CartProvider from './CartContext';
 import About from './pages/about';
 import Home from './pages';
 import Profile from './pages/profile';
-import ChatBot from 'react-simple-chatbot';
-import { ThemeProvider } from 'styled-components';
-import img from "./pages/images/no-profile.jpg";
+import Chatbot from './components/ChatBot';
 // localhost:3000 -> Home
 // localhost:3000/success -> Success
-import { Navbar } from 'react-bootstrap';
-const steps = [
-  {
-      id: '0',
-      message: 'Hey!',
-
-      trigger: '1',
-  }, {
-      id: '1',
-
-      message: 'Please write your username',
-      trigger: '2'
-  }, {
-      id: '2',
-
-
-      user: true,
-      trigger: '3',
-  }, {
-      id: '3',
-      message: " hi {previousValue}, how can I help you?",
-      trigger: 4
-  },{
-    id: '4',
-    options: [
-         
-
-        { value: 1, label: 'Order Status' },
-        { value: 2, label: 'Contact' },
-
-    ],
-    end: true
-}
-];
-const theme = {
-  background: '#C9FF8F',
-  headerBgColor: '#197B22',
-  headerFontSize: '20px',
-  botBubbleColor: '#0F3789',
-  headerFontColor: 'white',
-  botFontColor: 'white',
-  userBubbleColor: '#FF5733',
-  userFontColor: 'white',
-};
-
-// Set some properties of the bot
-const config = {
-  botAvatar: img,
-  floating: true,
-};
 
 
 function App() {
@@ -83,19 +31,9 @@ function App() {
             <Route path ="profile" index element={<Profile />} />
           </Routes>
         </BrowserRouter>
-        <ThemeProvider theme={theme}>
-                <ChatBot
- 
-                    // This appears as the header
-                    // text for the chat bot
-                    headerTitle="Contuso Bot"
-                    steps={steps}
-                    {...config}
- 
-                />
-            </ThemeProvider>
-      </Container>
 
+      </Container>
+      <Chatbot/>
     </CartProvider>
     
                 </>
---
+++ store/src/components/ChatBot.js
@@ -0,0 +1,74 @@
+import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
+import img from "../pages/images/no-profile.jpg";
+
+function Chatbot(){
+const steps = [
+    {
+        id: '0',
+        message: 'Hey! Do you need help?',
+        trigger: '1',
+    }, {
+        id: '1',
+  
+        message: 'Please write your username',
+        trigger: '2'
+    }, {
+        id: '2',
+  
+        user: true,
+        trigger: '3',
+    }, {
+        id: '3',
+        message: " hi {previousValue}, how can I help you?",
+        trigger: 4
+    }, {
+        id: '4',
+        options: [
+             
+            // When we need to show a number of
+            // options to choose we create alist
+            // like this
+            { value: 1, label: 'Order Status' },
+            { value: 2, label: 'Speak to a representative' },
+  
+        ],
+        end: true
+    }
+  ];
+  const theme = {
+    background: '#a2faf4',
+    headerBgColor: '#0F3789',
+    headerFontSize: '20px',
+    botBubbleColor: '#7e8584',
+    headerFontColor: 'white',
+    botFontColor: 'white',
+    userBubbleColor: '#0f74d9',
+    userFontColor: 'white',
+  };
+  
+  
+  
+  // Set some properties of the bot
+  const config = {
+    botAvatar: img,
+    floating: true,
+  };
+  return(
+    <div>
+            <ThemeProvider theme={theme}>
+                <ChatBot
+ 
+                    // This appears as the header
+                    // text for the chat bot
+                    headerTitle="Contuso Bot"
+                    steps={steps}
+                    {...config}
+ 
+                />
+            </ThemeProvider>
+    
+    </div>
+  )
+}
+export default Chatbot;
